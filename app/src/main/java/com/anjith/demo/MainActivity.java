4d9d0ff28e32da62c805cf1040f267500bdce423
Anjith Sasindran <anjithsasindran@BigAdmins-MacBook-Pro.local>
2017-10-27 22:59:03 +0530

    Add Sample activities
    Sample1Activity and Sample2Activity are added with their layout
    resource files. AndroidManifest is also updated.

--- app/src/main/java/com/anjith/demo/MainActivity.java
+++ app/src/main/java/com/anjith/demo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.anjith.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openSample(View view) {
+        switch (view.getId()) {
+            case R.id.button_sample_1:
+                startActivity(new Intent(this, Sample1Activity.class));
+                break;
+            case R.id.button_sample_2:
+                startActivity(new Intent(this, Sample2Activity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/anjith/demo/Sample1Activity.java
@@ -0,0 +1,19 @@
+package com.anjith.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anjithsasindran
+ * on 27/10/17.
+ */
+
+public class Sample1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_1);
+    }
+}
